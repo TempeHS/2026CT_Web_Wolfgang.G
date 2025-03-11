ed126cdcb410f74a2236ff03cf567a4a2fccd78e
Wolfgang G <wolfgang.gledhill@education.nsw.gov.au>
2025-03-11 00:56:26 +0000

    message

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+gameScore = "1";
+computerScore = 1;
+
+sumScore = gamerScore + computerScore;
+
+alert(sumScore);
+
+function myGame() {
+  player_name = prompt("enter your name");
+  alert("Please leave me alone " + player_name);
+  player_guess = prompt("Play scissors paper rock");
+}
