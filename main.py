eaf677b93348ff256d020f6fdd0a68476de321dd
Wolfgang G <wolfgang.gledhill@education.nsw.gov.au>
2025-05-27 23:39:29 +0000

    More Cards

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 @app.route('/index.html')
 @app.route('/')
 def index():
-        return render_template("index.html"), 200 
+        card_data = (
+                ("What is Carcinization", "Lorem Ipsum", "Take me there", "Static/images/logo.png"),
+                ("What is Carcinization", "Lorem Ipsum", "Take me there", "Static/images/logo.png"),
+                ("What is Carcinization", "Lorem Ipsum", "Take me there", "Static/images/logo.png"),
+                ("What is Carcinization", "Lorem Ipsum", "Take me there", "Static/images/logo.png"),
+                ("What is Carcinization", "Lorem Ipsum", "Take me there", "Static/images/logo.png"),
+                ("What is Carcinization", "Lorem Ipsum", "Take me there", "Static/images/logo.png"),
+        ) 
+        return render_template("index.html", cards=card_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
